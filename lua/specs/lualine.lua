4f3dad4d8ee4da7b33559c265fb6945602b091fd
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2021-10-08 15:01:57 +0600

    use same config as lightline in lualine for benchmark

--- lua/specs/lualine.lua
+++ lua/specs/lualine.lua
@@ -1,2 +1,17 @@
 vim.cmd([[packadd lualine.nvim]])
-require('lualine').setup()
+require('lualine').setup {
+  options = {
+    icons_enabled = false,
+    theme = 'powerline',
+    component_separators = '|',
+    section_separators = '',
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'filename' },
+    lualine_c = { },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
+}
